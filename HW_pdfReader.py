26ad49fcf917d4f5c3f106fa23cf6300d161e2b8
ruskintanoy <ruskintanoyy@gmail.com>
2024-10-02 08:26:39 -0600

    Update HW_pdfReader.py

--- HW_pdfReader.py
+++ HW_pdfReader.py
@@ -20,7 +20,7 @@ def pdf_to_excel(folder_path, output_excel_path):
         print("No PDF file found in the folder.")
         return
 
-    tables = camelot.read_pdf(pdf_path, flavor='stream', pages='')  # input to the page you want to copy
+    tables = camelot.read_pdf(pdf_path, flavor='stream', pages='')  # input the page num you want to copy
     df = tables[0].df   
     skip_conditions = ["SAMSUNG", "GOOGLE", "IPHONE", "BLACK", "SUMMARY", "USER"] 
     corrected_data = []
